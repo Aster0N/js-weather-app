6effb22c184e5497d115554c2c6a79483fbbea70
Aster0N <nikvas131@gmail.com>
2022-05-08 22:39:02 +0300

    Added fetch request after user input

--- src/app.js
+++ src/app.js
@@ -13,3 +13,23 @@ const maxTempField = document.querySelector('#max_temp')
 const minTempField = document.querySelector('#min_temp')
 const cloudsField = document.querySelector('#clouds')
 const showWeatherBtn = document.querySelector('#showWeatherBtn')
+
+
+async function getData(url) {
+	try {
+		const response = await fetch(url)
+		const data = await response.json()
+		return data
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+showWeatherBtn.addEventListener('click', () => {
+	if (userCityInput.value) {
+		let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + userCityInput.value + '&appid=0b0e367f470fb02507e7aa3e527cb404';
+		getData(url).then(data => {
+			console.log(data)
+		})
+	}
+})
