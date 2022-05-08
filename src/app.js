019c7703a31332aef59d9ffc020f85ac160c22f3
Aster0N <nikvas131@gmail.com>
2022-05-08 22:26:59 +0300

    Defined the minimum required variables for the weather data

--- src/app.js
+++ src/app.js
@@ -5,3 +5,11 @@ scrollDownBtn.addEventListener('click', e => {
 		behavior: 'smooth'
 	});
 })
+
+const userCityInput = document.querySelector('#userInput')
+const tempField = document.querySelector('#temp_field')
+const feelingField = document.querySelector('#feels_like')
+const maxTempField = document.querySelector('#max_temp')
+const minTempField = document.querySelector('#min_temp')
+const cloudsField = document.querySelector('#clouds')
+const showWeatherBtn = document.querySelector('#showWeatherBtn')
