f99db43b3e51abdfa6ba13ddb070f64da056048f
Aster0N <nikvas131@gmail.com>
2022-06-05 23:26:58 +0300

    Changed weather output fields IDs

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ scrollDownBtn.addEventListener('click', e => {
 })
 
 const userCityInput = document.querySelector('#userInput')
-const tempField = document.querySelector('#temp_field')
-const feelingField = document.querySelector('#feels_like')
-const maxTempField = document.querySelector('#max_temp')
-const minTempField = document.querySelector('#min_temp')
+const tempField = document.querySelector('#tempField')
+const feelingField = document.querySelector('#feelsLike')
+const maxTempField = document.querySelector('#maxTemp')
+const minTempField = document.querySelector('#minTemp')
 const cloudsField = document.querySelector('#clouds')
 const showWeatherBtn = document.querySelector('#showWeatherBtn')
 
